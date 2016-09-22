48e6b8716855f4e92b08e40d28abf3216d3dbfaf
zzswang@gmail.com <zzswang@gmail.com>
2016-09-22 17:06:31 +0800

    add test

---
+++ src/tests/store.test.js
@@ -0,0 +1,21 @@
+/**
+ * Test store addons
+ */
+
+import chai, { expect } from 'chai';
+import configureStore from '../store';
+
+
+describe('configureStore', () => {
+  let store;
+
+  before(() => {
+    store = configureStore();
+  });
+
+  describe('We have a workable store', () => {
+    it('store should be an object', () => {
+      expect(typeof store).to.equal('object');
+    });
+  });
+});
