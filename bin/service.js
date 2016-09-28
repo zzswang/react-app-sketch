eac4be01258173460848c12fe31dc149fe4c0947
zzswang@gmail.com <zzswang@gmail.com>
2016-09-28 10:43:40 +0800

    change to http-proxy

--- bin/service.js
+++ bin/service.js
@@ -3,31 +3,35 @@ import bodyParser from 'body-parser';
 import config from 'config';
 import expressJwt from 'express-jwt';
 import jwt from 'jsonwebtoken';
-import proxy from 'express-http-proxy';
+import httpProxy from 'http-proxy';
 
 
 const app = express();
 const secret = "whataawesomeday";
 
 app.set('port', config.service.port);
-app.use(bodyParser.json());
 
-// this is fake api, you could delete it if real one is ready
-app.post('/sessions', function(req, res) {
-  console.log('comming into /sessions')
-  const email = req.body.email || "anonymous";
-  const token = jwt.sign({ email, admin: email === 'admin@36node.com' }, secret);
-  return res.json({user:email, token, auth:true});
-});
+if(config.service.mock) {
+  app.use(bodyParser.json());
+  app.post('/sessions', function(req, res) {
+    console.log('comming into /sessions')
+    const email = req.body.email || "anonymous";
+    const token = jwt.sign({ email, admin: email === 'admin@36node.com' }, secret);
+    return res.json({user:email, token, auth:true});
+  });
 
-// this is fake api, you could delete it if real one is ready
-app.get('/protected/resources', expressJwt({secret}), function(req, res) {
-  if (!req.user.admin) return res.status(401).json({message: 'You are not admin!'});
-  return res.json({message:'mow'});
-});
+  app.get('/protected/resources', expressJwt({secret}), function(req, res) {
+    if (!req.user.admin) return res.status(401).json({message: 'You are not admin!'});
+    return res.json({message:'mow'});
+  });
+}
 
 // proxy to api service
-app.all('*', proxy(config.service.api_gateway));
+const proxy = httpProxy.createProxyServer();
+app.use((req, res) => {
+  console.log('proxy path: ', req.path);
+  proxy.web(req, res, { target: config.service.api_gateway });
+});
 
 
 export default app;
