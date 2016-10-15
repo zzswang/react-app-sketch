77240efcf5d0243171666b3cd16a45f72830d2bb
zzswang <zzswang@gmail.com>
2016-10-15 15:46:17 +0800

    export myHistory

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ import {browserHistory} from 'react-router';
 
 export const {baseUrl, version} = JSON.parse(document.getElementById('app-config').innerHTML);
 
-const history = useBasename(() => browserHistory)({basename: `/${baseUrl}`});
-export {history as myHistory};
+export const myHistory = useBasename(() => browserHistory)({basename: `/${baseUrl}`});
