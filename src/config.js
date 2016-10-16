09c8e844eea94314eadedc27579c1d869fdac06e
zzswang@gmail.com <zzswang@gmail.com>
2016-10-16 11:25:33 +0800

    change config for mocha test

--- src/config.js
+++ src/config.js
@@ -1,6 +1,22 @@
 import useBasename from 'history/lib/useBasename';
 import {browserHistory} from 'react-router';
 
-export const {baseUrl, version} = JSON.parse(document.getElementById('app-config').innerHTML);
+const initConfig = {
+  baseUrl: '/',
+  version: '1.0.0',
+};
 
+// get config from index.html
+function getConfigFromIndexHtml() {
+  let c = {};
+
+  if (global.document && document.getElementById('app-config')) {
+    c = JSON.parse(document.getElementById('app-config').innerHTML);
+  }
+
+  return Object.assign(initConfig, c);
+}
+
+
+export const {baseUrl, version} = getConfigFromIndexHtml();
 export const myHistory = useBasename(() => browserHistory)({basename: `/${baseUrl}`});
